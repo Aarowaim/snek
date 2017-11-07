f7e8c757bdebff53e472c7084ba72f7229899a3d
Aaron Lip <kitsunay34@gmail.com>
2017-11-06 19:44:10 -0700

    Initial Commit

---
+++ final.py
@@ -0,0 +1,123 @@
+import time
+import random
+
+from decimal import Decimal # The accurate decimal type (TM)
+
+import uagame
+import pygame
+
+from pygame.locals import *
+from pygame.math import Vector2
+
+from objects import *
+
+
+def main():
+
+    window = uagame.Window('Snake', 800, 800)
+    window.set_auto_update(False)
+    game = Game(window)
+    game.play()
+    window.close()
+
+
+class Game(object):
+
+    def __init__(self, window, pause_time=1/60):
+        pygame.key.set_repeat(20, 20)
+
+        self.window = window
+        self.pause_time = pause_time
+
+        self.close_clicked = False
+        self.continue_game = True  # whether game logic should run
+
+        # This assignment is too small to justify decoupling collisions
+        self.snake = Snake(window.get_surface(), 100, 100, {'left' : K_a, 'right' : K_d})
+        self.eco = Ecosystem(window.get_surface())
+
+        self.game_timer = Timer()
+        self.time_display = NumberDisplay(
+            window.get_width() / 2,
+            window.get_height() - window.get_font_height() - 10,
+            size=24,
+            center=True
+        )
+        self.score_display = NumberDisplay(
+            0,
+            0,
+            size=24,
+            label='Score: '
+        )
+
+        self.entities = [
+            self.time_display,
+            self.score_display,
+            self.snake,
+            self.eco
+        ]
+
+    def draw(self):
+        self.window.clear()
+
+        for e in self.entities:
+            e.draw(self.window)
+
+        self.window.update()
+
+    def update(self):
+        # Update the counter to show how long the match has been
+        self.game_timer.update()
+        self.time_display.amount = round(self.game_timer.elapsed(), 1)
+
+        for e in self.entities:
+            e.update()
+
+        self.snake.max_length += self.eco.check_eaten(self.snake.head())
+        self.score_display.amount = self.snake.length()
+
+    def play(self):
+
+        logic_time = Timer()
+        logic_step = Decimal(1/60)
+
+        render_time = Timer()
+        render_step = Decimal(self.pause_time)
+
+        while not self.close_clicked:
+
+            self.handle_event()
+
+            # Catch up logic state by consuming fixed units of time
+            while logic_time.elapsed() > logic_step:
+                if self.continue_game:
+                    self.update()
+                    self.continue_game = self.decide_continue()
+
+                logic_time.consume(logic_step)
+
+            # Render state gets updated to meet the framerate of render_step
+            if render_time.elapsed() > render_step:
+                render_time.clear(render_step)
+                self.draw()
+
+            time.sleep(self.pause_time)
+
+    def handle_event(self):
+        event = pygame.event.poll()
+
+        if event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
+            self.close_clicked = True
+
+        if self.continue_game:
+            for e in self.entities:
+                e.handle(event)
+
+    def decide_continue(self):
+        x = self.snake.head().x
+        y = self.snake.head().y
+        return not self.snake.overlaps() and \
+            0 < x and x < self.window.get_width()and \
+            0 < y and y < self.window.get_height()
+
+main()
---
+++ objects.py
@@ -0,0 +1,210 @@
+import time
+import random
+import math
+
+from decimal import Decimal
+
+import pygame
+
+from pygame.locals import *
+from pygame.math import Vector2
+
+from geometry import calculateIntersectPoint;
+
+class Entity(object):
+
+    def draw(self, window):  # rendering tick
+        pass
+
+    def update(self, collisions=[]):  # logic/physics tick
+        pass
+
+    def handle(self, event): # event tick
+        pass
+
+
+class Timer(Entity):
+
+    def __init__(self):
+        self.is_paused = False
+        self.__elapsed = Decimal(0)
+        self.__last = self.now()
+
+    def now(self):
+        return Decimal(time.perf_counter())
+
+    def elapsed(self):
+        now = self.now()  # avoid losing time by caching
+
+        if not self.is_paused:
+            self.__elapsed += now - self.__last
+        self.__last = now
+        return self.__elapsed
+
+    def consume(self, time=None):
+        if time:
+            self.__elapsed -= Decimal(time)
+        else:
+            self.__elapsed = 0
+
+    def clear(self, time=None):
+        if time:
+            self.__elapsed %= time
+        else:
+            self.__elapsed = 0
+
+    def pause(self):
+        self.is_paused = True
+
+    def resume(self):
+        self.is_paused = False
+
+class Ecosystem(Entity):
+
+    def __init__(self, surface, food_rate=5, food_max=20):
+        self.food_rate = food_rate
+        self.food_max = food_max
+        self.limits = Vector2(surface.get_width(), surface.get_height())
+
+        self.food_timer = Timer()
+
+        self.foods = {}
+        self.make_food()
+
+    def draw(self, window):
+        for f in self.foods.values():
+            f.draw(window)
+
+    def update(self, collisions=[]):
+        if self.food_timer.elapsed() > self.food_rate:
+            self.food_timer.consume(self.food_rate)
+            self.make_food()
+            
+    def make_food(self):
+        if len(self.foods) < self.food_max:
+            point = self.randpoint()
+            while (int(point.x), int(point.y)) in self.foods:
+                point = self.randpoint()
+            self.foods[(int(point.x), int(point.y))] = Food(point.x, point.y)
+
+    def randpoint(self):
+        return Vector2(random.randint(0, self.limits.x), random.randint(0, self.limits.x))
+
+    def check_eaten(self, vector_point):
+        new_foods = {}
+        eaten = 0
+        for pos, f in self.foods.items():
+            if not f.is_inside(vector_point):
+                new_foods[pos] = f
+            else:
+                eaten += math.pi * f.radius ** 2
+        self.foods = new_foods
+        return eaten
+
+
+class Food(Entity):
+
+    def __init__(self, x, y, radius=6):
+        self.radius = radius
+        self.position = Vector2(x, y)
+
+    def draw(self, window, patterns=set()): 
+        self.draw_circle(window, *self.position)
+
+    def draw_circle(self, window, x, y):
+        pygame.draw.circle(
+            window.get_surface(), 
+            Color('white'),
+            [int(val) for val in [x, y]],
+            self.radius
+        )
+
+    def is_inside(self, vector_point):
+        return self.position.distance_to(vector_point) <= self.radius
+
+    def update(self, collisions=[]):
+        pass
+
+class Snake(Entity):
+    def __init__(self, surface, x, y, keys, movescale=3.5):
+        self.position = Vector2(x, y)
+        self.velocity = Vector2(1, 0.5)
+        self.velocity.scale_to_length(2)
+        self.keys = keys
+        self.movescale = movescale
+
+        self.limits = Vector2(surface.get_width(), surface.get_height())
+
+        self.max_length = 100
+        self.segments = [self.position]
+
+    def draw(self, window):
+        colors = [pygame.Color(c) for c in ['green', 'yellow', 'orange', 'red', 'violet', 'blue']]
+        c_i = 0
+        if len(self.segments) > 1:
+            for i in range(len(self.segments) - 1):
+                if self.segments[i].distance_to(self.segments[i+1]) < 10:
+                    pygame.draw.line(
+                        window.get_surface(), 
+                        colors[c_i], 
+                        self.segments[i],
+                        self.segments[i+1],
+                        2
+                    )
+                c_i = (c_i + 1) % len(colors)
+                    
+
+    def update(self, collisions=[]):
+        pressed = pygame.key.get_pressed()
+        offset_x = -pressed[self.keys['left']] +pressed[self.keys['right']]
+
+        self.velocity.rotate_ip(self.movescale * offset_x)
+
+        next_pos = self.position + self.velocity
+        if self.head().distance_to(next_pos) > 1:
+            self.segments.insert(0, next_pos)
+
+        if self.length() >= self.max_length:
+            self.segments.pop()
+
+        self.position += self.velocity
+
+    def length(self):
+        accumulator = 0
+        for i in range(len(self.segments) - 1):
+            accumulator += self.segments[i].distance_to(self.segments[i+1])
+        return accumulator
+
+    def head(self):
+        return self.segments[0]
+
+    # TODO: Linear Algebra
+    def overlaps(self):
+        s = self.segments
+        if len(self.segments) > 5:
+            for i in range(4, len(s) - 1):
+                if calculateIntersectPoint(s[0], s[1], s[i], s[i+1]):
+                    return True
+        return False
+
+
+class NumberDisplay(Entity):
+
+    def __init__(self, x, y, amount=0, color='white', size=48, label='', center=False):
+        self.position = Vector2(x, y)
+        self.color = color
+        self.size = size
+        self.amount = amount
+        self.label = label
+        self.center = center
+
+    def draw(self, window):
+        window.set_font_size(self.size)
+        window.set_font_color(self.color)
+
+        text = self.label + str(round(self.amount, 1))
+        x_pos = self.position.x
+        if self.center:
+            x_pos = self.position.x - window.__font__.size(text)[0] / 2
+
+        window.draw_string(text, x_pos, self.position.y)
